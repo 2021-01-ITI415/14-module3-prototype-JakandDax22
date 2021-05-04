fde02d1f2a4ec2625bfa97ad09b982bccb8eaf3d
JakandDax22 <60167934+JakandDax22@users.noreply.github.com>
2021-05-04 18:08:56 -0400

    Designed terrain with interior and pickups

---
+++ Assets/Scripts/PickupCounter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class PickupCounter : MonoBehaviour
+{
+   //public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+ 
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        setCountText();
+        winTextObject.SetActive(false);
+    }
+
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+            {
+            winTextObject.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+}
